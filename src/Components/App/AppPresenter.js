1ef4535bf0ee6234d4dbb4ef93fe9c5694f7ed07
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-11 15:22:07 +0900

    #8 Giving props to the Routes

--- src/Components/App/AppPresenter.js
+++ src/Components/App/AppPresenter.js
@@ -23,16 +23,33 @@ const Main = styled.main`
   }
 `;
 
-const AppPresenter = ({ isLoading, transations, blocks }) => (
+const AppPresenter = ({ isLoading, transactions, blocks }) => (
   <BrowserRouter>
     <AppContainer>
       <Header />
       {!isLoading && (
         <Main>
           <Switch>
-            <Route exact path={`/`} component={Home} />
-            <Route exact path={`/blocks`} component={Blocks} />
-            <Route exact path={`/transactions`} component={Transactions} />
+            <Route
+              exact
+              path={`/`}
+              render={() => (
+                <Home
+                  blocks={blocks.slice(0, 5)}
+                  transactions={transactions.slice(0, 5)}
+                />
+              )}
+            />
+            <Route
+              exact
+              path={`/blocks`}
+              render={() => <Blocks blocks={blocks} />}
+            />
+            <Route
+              exact
+              path={`/transactions`}
+              render={() => <Transactions transactions={transactions} />}
+            />
           </Switch>
         </Main>
       )}
@@ -41,7 +58,9 @@ const AppPresenter = ({ isLoading, transations, blocks }) => (
 );
 
 AppPresenter.propTypes = {
-  isLoading: PropTypes.bool.isRequired
+  isLoading: PropTypes.bool.isRequired,
+  transactions: PropTypes.array,
+  blocks: PropTypes.array
 };
 
 export default AppPresenter;
