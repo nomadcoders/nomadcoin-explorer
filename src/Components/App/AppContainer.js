4312eb935f266fb70894e3ac283f3f8eb0e549b4
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-11 17:48:54 +0900

    #12 Updating the Data on Real Time

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -43,6 +43,15 @@ class AppContainer extends Component {
     const ws = new WebSocket(WS_URL);
     ws.addEventListener("message", message => {
       const parsedMessage = parseMessage(message);
+      if (parsedMessage !== null && parsedMessage !== undefined) {
+        this.setState(prevState => {
+          return {
+            ...prevState,
+            blocks: [...parsedMessage, ...prevState.blocks],
+            transactions: [...parsedMessage[0].data, ...prevState.transactions]
+          };
+        });
+      }
     });
   };
 }
